474ed30b6f4f357fe6ef876ed522ea2e5f7e9e80
soorokim <rlatnfh99@gmail.com>
2025-01-04 18:07:45 +0900

    test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
+const { CLOCK, TEST } = process.env;
+
 const nextConfig: NextConfig = {
   /* config options here */
+  rewrites: async () => [
+    {
+      source: "/blog",
+      destination: `${CLOCK}`,
+    },
+    {
+      source: "/assets/:path+",
+      destination: `${CLOCK}/assets/:path+`,
+    },
+    {
+      source: "/test",
+      destination: `${TEST}`,
+    },
+    {
+      source: "/_next/static/:path+",
+      destination: `${TEST}/_next/static/:path+`,
+    },
+  ],
 };
 
 export default nextConfig;
